31af51852a657e1e9ddb3592b35dcfb1d8b2891a
Danielle Souza <dany.sa1@hotmail.com>
2023-04-09 14:47:50 -0300

    feat: add tag

---
+++ stories/AbTag.stories.tsx
@@ -0,0 +1,17 @@
+
+import React from "react";
+ import { AbTag, AbTagProps } from '../src/components/AbTag'
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+
+export default {
+    title: 'Component/AbTag',
+    component: AbTag
+} as ComponentMeta<typeof AbTag>
+
+const Template: ComponentStory<typeof AbTag> = (args) => <AbTag {...args}/>
+
+export const Primario = Template.bind({})
+
+Primario.args = {
+    texto: 'Tag'
+} as AbTagProps
